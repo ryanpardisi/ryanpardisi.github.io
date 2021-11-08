88135addbc0e51e0713633953c47e88acfdf5763
Ryan Pardisi <63972514+ryanpardisi@users.noreply.github.com>
2021-11-08 19:17:38 +0330

    Add files via upload

--- static/scripts/js/actions.js
+++ static/scripts/js/actions.js
@@ -8,4 +8,16 @@ $(function(){
     $(".goto-github").click(function(){
         window.open("https://github.com/ryanpardisi");
     });
+
+    $(".goto-instagram").click(function(){
+        window.open("https://instagram.com/ryanpardisi");
+    });
+
+    $(".goto-facebook").click(function(){
+        window.open("https://facebook.com/ryanpardisi");
+    });
+
+    $(".goto-telegram").click(function(){
+        window.open("https://t.me/ryanpardisi");
+    });
 });
